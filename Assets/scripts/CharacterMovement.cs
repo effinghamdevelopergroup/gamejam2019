65ce6708007cc156303b7585266c0d3dac41531d
Logan Malcome <lmalcome@lakelandcollege.edu>
2019-01-26 00:44:55 -0600

    Added idle anim

--- Assets/scripts/CharacterMovement.cs
+++ Assets/scripts/CharacterMovement.cs
@@ -16,20 +16,33 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        anim.SetBool("move", false);
+
         Vector3 pos = transform.position;
         if (Input.GetKey("w"))
+        {
+            anim.SetBool("move", true);
             pos.z += MoveRate;
+        }
+            
         if (Input.GetKey("s"))
+        {
+            anim.SetBool("move", true);
             pos.z -= MoveRate;
+        }
+            
         if (Input.GetKey("a"))
         {
             pos.x -= MoveRate;
+            anim.SetBool("move", true);
             UpdateAnimations(-1);
         }
             
         if (Input.GetKey("d"))
         {
             pos.x += MoveRate;
+            anim.SetBool("move", true);
             UpdateAnimations(1);
         }
             
