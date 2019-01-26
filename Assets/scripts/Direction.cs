a3e76896a0b8faf59376cb7f40d030d4cf1243f3
Scott Stoneking <scott.stoneking@jjventures.com>
2019-01-26 14:50:14 -0600

    working on arrow warning

---
+++ Assets/scripts/Direction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.scripts
+{
+    public enum Direction
+    {
+        East,
+        West,
+        South,
+        North
+    }
+}
--- Assets/scripts/Negative.cs
+++ Assets/scripts/Negative.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.scripts;
 
 public class Negative : MonoBehaviour
 {
@@ -9,17 +10,32 @@ public class Negative : MonoBehaviour
     public int Damage = 1;
     public int Experience = 1;
     public GameObject deathFx;
+    private Vector3 startPos;
+    private Direction startDir;
+    public GameObject Warning;
+    private GameObject warning;
+
     // Start is called before the first frame update
     void Start()
     {
         _globalProps = GameObject.FindGameObjectWithTag("Global").GetComponent<GameController>();
+        startPos = transform.position;
+        //if (startPos.x < 0 && startPos.y < 0)
+        //{
+        //    startDir = Direction.West;
+        //} else if (startPos.x > 0 )
+        //{
+        //    startDir = Direction.West;
+        //}
+        //_globalProps = 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 house = GameObject.FindGameObjectWithTag("House").transform.position;
         float step = Speed * Time.deltaTime;
+        Vector3 house = GameObject.FindGameObjectWithTag("House").transform.position;
+        
         transform.position = Vector3.MoveTowards(transform.position, house, step);
     }
 
@@ -35,11 +51,26 @@ public class Negative : MonoBehaviour
             _globalProps.Score -= Damage;
             Destroy(gameObject);
         }
+        if (other.gameObject.tag == "Proximity")
+        {
+            warning = CreateWarning();
+        }
     }
 
     private void OnDestroy()
     {
-        Instantiate(deathFx, transform.position,Quaternion.identity);
+        Instantiate(deathFx, transform.position, Quaternion.identity);
+        Destroy(warning);
         // Show particle animation
     }
+
+    private GameObject CreateWarning()
+    {
+        GameObject player = GameObject.Find("Player");
+        GameObject newWarning = Instantiate(Warning, player.transform.position, Quaternion.identity);
+        newWarning.GetComponent<WarningArrow>().Negative = gameObject;
+        newWarning.transform.parent = player.transform;
+        return newWarning;
+    }
+
 }
